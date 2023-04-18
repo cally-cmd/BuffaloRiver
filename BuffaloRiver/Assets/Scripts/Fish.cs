9c7fd784649bbc49aed41cadb719769d9ad09b32
Cally <burnettdt@hendrix.edu>
2023-04-18 10:10:36 -0500

    Design layout, and prefabs

--- BuffaloRiver/Assets/Scripts/Fish.cs
+++ BuffaloRiver/Assets/Scripts/Fish.cs
@@ -8,7 +8,8 @@ public class Fish : MonoBehaviour
     private float x;
     private float y;
     private float frequency;
-    public GameObject fish;
+    public GameObject fish1;
+    public GameObject fish2;
     private bool jumping;
     private float amplitude;
     void Start()
@@ -19,22 +20,22 @@ public class Fish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // float now = Time.time;
-        // if (jumping) {
-        //     float stop = now + Mathf.PI;
-        //     while (Time.time < stop) {
-        //         x = Mathf.Cos(Time.time * frequency) * amplitude;
-        //         y = Mathf.Sin(Time.time * frequency) * amplitude;
+        float now = Time.time;
+        if (jumping) {
+            float stop = now + Mathf.PI;
+            while (Time.time < stop) {
+                x = Mathf.Cos(Time.time * frequency) * amplitude;
+                y = Mathf.Sin(Time.time * frequency) * amplitude;
 
-        //         fish.transform.position = new Vector2(x, y);
-        //     }
-        //     jumping = false;
-        //     Destroy(fish);
-        // }
+                fish1.transform.position = new Vector2(x, y);
+            }
+            jumping = false;
+            Destroy(fish1);
+        }
     }
 
     public void Jump() {
-        Instantiate(fish);
+        Instantiate(fish1);
         jumping = true;
         printTest();
     }
