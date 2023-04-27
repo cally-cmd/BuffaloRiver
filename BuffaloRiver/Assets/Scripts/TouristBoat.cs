626408d1f173de90ad4e8850f3b72604919ad8b5
Cally <daniel.burnett.314@gmail.com>
2023-04-27 10:29:03 -0500

    Boat audio trigger on buy

---
+++ BuffaloRiver/Assets/Scripts/TouristBoat.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouristBoat : MonoBehaviour {
+
+  public AudioSource clip;
+
+  public void Play() {
+    clip.Play();
+  }
+}
